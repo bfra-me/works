2a184f526274d5d32b1c374df51eab4ce9625a40
Marcus R. Brown <contact@marcusrbrown.com>
2024-02-27 22:00:24 -0700

    chore(tsconfig): format

--- packages/tsconfig/test/schema.test.ts
+++ packages/tsconfig/test/schema.test.ts
@@ -1,21 +1,21 @@
-import assert from 'node:assert';
-import {promises as fs} from 'node:fs';
-import {test} from 'node:test';
-import ajv, {type JSONSchemaType} from 'ajv-draft-04';
-import type {TsConfigJson} from 'type-fest';
+import assert from 'node:assert'
+import {promises as fs} from 'node:fs'
+import {test} from 'node:test'
+import ajv, {type JSONSchemaType} from 'ajv-draft-04'
+import type {TsConfigJson} from 'type-fest'
 
-const Ajv = ajv.default;
+const Ajv = ajv.default
 
 test('validates against tsconfig.json schema', async () => {
-  const tsconfig = JSON.parse(await fs.readFile('tsconfig.json', 'utf8'));
+  const tsconfig = JSON.parse(await fs.readFile('tsconfig.json', 'utf8'))
   const schema = (await (
     await fetch('https://json.schemastore.org/tsconfig')
-  ).json()) as JSONSchemaType<TsConfigJson>;
+  ).json()) as JSONSchemaType<TsConfigJson>
   const ajv = new Ajv({
     strict: false,
     keywords: ['allowTrailingCommas', 'markdownDescription', 'markdownEnumDescriptions'],
-  });
-  const validate = ajv.compile(schema);
+  })
+  const validate = ajv.compile(schema)
 
-  assert(validate(tsconfig), 'tsconfig.json does not validate against the schema');
-});
+  assert(validate(tsconfig), 'tsconfig.json does not validate against the schema')
+})
