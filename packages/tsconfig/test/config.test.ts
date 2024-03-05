cc549df7e2453c7b38e8e8c050e39ba8868a5988
Marcus R. Brown <contact@marcusrbrown.com>
2024-03-05 07:08:52 -0700

    chore(tsconfig): update tests (#142)

--- packages/tsconfig/test/config.test.ts
+++ packages/tsconfig/test/config.test.ts
@@ -1,23 +1,25 @@
 import assert from 'node:assert'
 import path from 'node:path'
-import {test} from 'node:test'
+import {describe, it} from 'node:test'
 import ts from 'typescript'
 
 const {sys, findConfigFile, readConfigFile, parseJsonConfigFileContent} = ts
 
-test('contains valid configuration', async () => {
-  const tsconfigPath = findConfigFile(process.cwd(), sys.fileExists)
-  if (!tsconfigPath) {
-    throw new Error('No tsconfig.json file found')
-  }
-  const {config: tsconfig, error} = readConfigFile(tsconfigPath, sys.readFile)
-  assert(error === undefined, `Error reading tsconfig.json: ${error?.messageText}`)
+describe('config', () => {
+  it('contains valid configuration', async () => {
+    const tsconfigPath = findConfigFile(process.cwd(), sys.fileExists)
+    if (!tsconfigPath) {
+      throw new Error('No tsconfig.json file found')
+    }
+    const {config: tsconfig, error} = readConfigFile(tsconfigPath, sys.readFile)
+    assert(error === undefined, `Error reading tsconfig.json: ${error?.messageText}`)
 
-  const parsed = parseJsonConfigFileContent(tsconfig, sys, path.dirname(tsconfigPath))
-  assert(
-    parsed.errors.length === 0,
-    `Error parsing tsconfig.json: ${parsed.errors.map((e) => e.messageText).join(', ')}`,
-  )
+    const parsed = parseJsonConfigFileContent(tsconfig, sys, path.dirname(tsconfigPath))
+    assert(
+      parsed.errors.length === 0,
+      `Error parsing tsconfig.json: ${parsed.errors.map(e => e.messageText).join(', ')}`,
+    )
 
-  assert(parsed.options.target === ts.ScriptTarget.ESNext, 'target must be ESNext')
+    assert(parsed.options.target === ts.ScriptTarget.ESNext, 'target must be ESNext')
+  })
 })
