eacecb274cf72190d8ed087a97583496d2dd69a7
Marcus R. Brown <contact@marcusrbrown.com>
2024-03-19 02:49:56 -0700

    chore: move `defineConfig` to a separate module

--- packages/semantic-release/src/index.ts
+++ packages/semantic-release/src/index.ts
@@ -1,13 +1,2 @@
-import type {SemanticReleaseConfig} from './types'
-
-/**
- * Define semantic-release global config.
- *
- * @param config Semantic Release configuration
- * @returns Semantic Release configuration
- */
-export function defineConfig(config: SemanticReleaseConfig): SemanticReleaseConfig {
-  return config
-}
-
+export {defineConfig} from './config'
 export type * from './types'
