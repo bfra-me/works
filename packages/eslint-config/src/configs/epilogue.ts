d9989669c772e8d01d845884f0c0b1d8daa32bf8
Marcus R. Brown <git@mrbro.dev>
2024-10-05 01:48:34 -0700

    feat(eslint-config): add epilogue configs (#434)

--- packages/eslint-config/src/configs/epilogue.ts
+++ packages/eslint-config/src/configs/epilogue.ts
@@ -1,15 +1,40 @@
+import {GLOB_SRC, GLOB_SRC_EXT} from '../globs'
 import type {Config} from '../types'
 
 export async function epilogue(): Promise<Config[]> {
+  // @keep-sorted {"keys":["name"]}
   return [
+    {
+      name: '@bfra.me/epilogue/commonjs',
+      files: ['**/*.js', '**/*.cjs'],
+      rules: {
+        '@typescript-eslint/no-require-imports': 'off',
+      },
+    },
+    {
+      name: '@bfra.me/epilogue/configs',
+      files: [`**/*.config.${GLOB_SRC_EXT}`, `**/*.config.*.${GLOB_SRC_EXT}`],
+      rules: {
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        'no-console': 'off',
+      },
+    },
     {
       name: '@bfra.me/epilogue/dts',
       files: ['**/*.d.?([cm])ts'],
       rules: {
         'eslint-comments/no-unlimited-disable': 'off',
-        'import/no-duplicates': 'off',
+        'import-x/no-duplicates': 'off',
         'no-restricted-syntax': 'off',
         'unused-imports/no-unused-vars': 'off',
+      },
+    },
+    {
+      name: '@bfra.me/epilogue/scripts',
+      files: [`**/scripts/${GLOB_SRC}`],
+      rules: {
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        'no-console': 'off',
       },
     },
   ]
