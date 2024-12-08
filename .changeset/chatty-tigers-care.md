3ffcf12b40a17911e3af4eb68930a03d2db8360c
Marcus R. Brown <git@mrbro.dev>
2024-12-08 05:44:09 -0700

    feat(eslint-config): support linting JSON, JSON5, and JSONC files (#726)

---
+++ .changeset/chatty-tigers-care.md
@@ -0,0 +1,6 @@
+---
+"@bfra.me/eslint-config": minor
+---
+
+Add a preset to handle linting JSON, JSON5, and JSONC files.
+  
---
+++ packages/eslint-config/src/configs/jsonc.ts
@@ -0,0 +1,31 @@
+import type {Config} from '../config'
+import type {Flatten, OptionsFiles, OptionsOverrides} from '../options'
+import {GLOB_JSON, GLOB_JSON5, GLOB_JSONC} from '../globs'
+import {interopDefault} from '../plugins'
+
+export type JsoncOptions = Flatten<OptionsFiles & OptionsOverrides>
+
+export async function jsonc(options: JsoncOptions): Promise<Config[]> {
+  const {files = [GLOB_JSONC, GLOB_JSON, GLOB_JSON5], overrides = {}} = options
+  const [pluginJsonc, parserJsonc] = await Promise.all([
+    interopDefault(import('eslint-plugin-jsonc')),
+    interopDefault(import('jsonc-eslint-parser')),
+  ])
+
+  return [
+    {
+      name: '@bfra.me/jsonc/plugins',
+      plugins: {jsonc: pluginJsonc as any},
+    },
+    {
+      name: '@bfra.me/jsonc',
+      files,
+      languageOptions: {
+        parser: parserJsonc,
+      },
+      rules: {
+        ...overrides,
+      },
+    },
+  ]
+}
