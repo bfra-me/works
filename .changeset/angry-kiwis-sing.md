5f2c4d6105d66ba2b746ad3a6d87eedbdf7129a9
Marcus R. Brown <git@mrbro.dev>
2024-12-25 03:24:36 -0700

    feat(eslint-config)!: migrate more config presets to use `requireOf`

---
+++ .changeset/angry-kiwis-sing.md
@@ -0,0 +1,5 @@
+---
+"@bfra.me/eslint-config": minor
+---
+
+Migrate more config presets to use `requireOf`.
--- packages/eslint-config/src/configs/node.ts
+++ packages/eslint-config/src/configs/node.ts
@@ -1,24 +1,33 @@
 import type {Config} from '../config'
-import {node as pluginNode} from '../plugins'
+import {interopDefault} from '../plugins'
+import {requireOf} from '../require-of'
+import {fallback} from './fallback'
 
 export async function node(): Promise<Config[]> {
-  return [
-    {
-      name: '@bfra.me/node',
-      plugins: {
-        node: pluginNode,
-      },
-      rules: {
-        'node/handle-callback-err': ['error', '^(err|error)$'],
-        'node/no-deprecated-api': 'error',
-        'node/no-exports-assign': 'error',
-        'node/no-new-require': 'error',
-        'node/no-path-concat': 'error',
-        'node/no-unsupported-features/es-builtins': 'error',
-        'node/prefer-global/buffer': ['error', 'never'],
-        'node/prefer-global/process': ['error', 'never'],
-        'node/process-exit-as-throw': 'error',
-      },
+  return requireOf(
+    ['eslint-plugin-n'],
+    async () => {
+      const pluginNode = await interopDefault(import('eslint-plugin-n'))
+      return [
+        {
+          name: '@bfra.me/node',
+          plugins: {
+            node: pluginNode,
+          },
+          rules: {
+            'node/handle-callback-err': ['error', '^(err|error)$'],
+            'node/no-deprecated-api': 'error',
+            'node/no-exports-assign': 'error',
+            'node/no-new-require': 'error',
+            'node/no-path-concat': 'error',
+            'node/no-unsupported-features/es-builtins': 'error',
+            'node/prefer-global/buffer': ['error', 'never'],
+            'node/prefer-global/process': ['error', 'never'],
+            'node/process-exit-as-throw': 'error',
+          },
+        },
+      ]
     },
-  ]
+    fallback,
+  )
 }
