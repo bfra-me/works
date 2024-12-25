7e6d1372ce52561c862f3c461f2967bf35e1e053
Marcus R. Brown <git@mrbro.dev>
2024-12-25 00:02:22 -0700

    feat(eslint-config)!: refactor jsonc, toml, and yaml configs to use `require-of`

--- packages/eslint-config/src/configs/toml.ts
+++ packages/eslint-config/src/configs/toml.ts
@@ -1,12 +1,16 @@
 import type {Config} from '../config'
 import type {Flatten, OptionsFiles, OptionsOverrides, OptionsPrettier} from '../options'
-import {GLOB_TOML} from '../globs'
+import {anyParser} from '../parsers/any-parser'
 import {interopDefault} from '../plugins'
+import {requireOf} from '../require-of'
+import {fallback} from './fallback'
 
 /**
  * Represents the options for configuring TOML files in the ESLint configuration.
  */
 export type TomlOptions = Flatten<OptionsFiles & OptionsOverrides & OptionsPrettier>
+
+export const tomlFiles = ['*.toml'].flatMap(p => [p, `**/${p}`])
 
 /**
  * Configures the ESLint rules for TOML files.
@@ -14,43 +18,43 @@ export type TomlOptions = Flatten<OptionsFiles & OptionsOverrides & OptionsPrett
  * @see https://ota-meshi.github.io/eslint-plugin-toml/
  */
 export async function toml(options: TomlOptions = {}): Promise<Config[]> {
-  const {files = [GLOB_TOML], overrides = {}, prettier = true} = options
-  const [pluginToml, parserToml] = await Promise.all([
-    interopDefault(import('eslint-plugin-toml')),
-    interopDefault(import('toml-eslint-parser')),
-  ])
+  const {files = tomlFiles, overrides = {}, prettier = true} = options
+  return requireOf(
+    ['eslint-plugin-toml'],
+    async () => {
+      const pluginToml = await interopDefault(import('eslint-plugin-toml'))
 
-  return [
-    {
-      name: '@bfra.me/toml/plugins',
-      plugins: {toml: pluginToml as any},
+      return [
+        ...(pluginToml.configs['flat/standard'] as Config[]),
+        {
+          name: '@bfra.me/toml',
+          files,
+          rules: {
+            ...(prettier
+              ? {
+                  // TODO: Detect if the TOML plugin for Prettier is installed
+                  // and if so, use the Prettier rules
+                  'prettier/prettier': 'off',
+
+                  'toml/array-bracket-newline': 'off',
+                  'toml/array-bracket-spacing': 'off',
+                  'toml/array-element-newline': 'off',
+                  'toml/indent': 'off',
+                  'toml/inline-table-curly-spacing': 'off',
+                  'toml/key-spacing': 'off',
+                  'toml/table-bracket-spacing': 'off',
+                }
+              : {}),
+
+            ...overrides,
+          },
+        },
+      ]
     },
-    {
-      name: '@bfra.me/toml',
-      files,
-      languageOptions: {
-        parser: parserToml,
-      },
-      rules: {
-        ...(pluginToml.configs.standard.rules as OptionsOverrides['overrides']),
-        ...(prettier
-          ? {
-              // TODO: Detect if the TOML plugin for Prettier is installed
-              // and if so, use the Prettier rules
-              'prettier/prettier': 'off',
-
-              'toml/array-bracket-newline': 'off',
-              'toml/array-bracket-spacing': 'off',
-              'toml/array-element-newline': 'off',
-              'toml/indent': 'off',
-              'toml/inline-table-curly-spacing': 'off',
-              'toml/key-spacing': 'off',
-              'toml/table-bracket-spacing': 'off',
-            }
-          : {}),
-
-        ...overrides,
-      },
-    },
-  ]
+    async missingList =>
+      fallback(missingList, {
+        files,
+        languageOptions: {parser: anyParser},
+      }),
+  )
 }
