d1828e73fe03c5bce8999cc70d9337883a25a11b
Marcus R. Brown <git@mrbro.dev>
2025-04-13 03:49:57 -0700

    chore(create): disable console warnings from template source files

--- packages/create/eslint.config.ts
+++ packages/create/eslint.config.ts
@@ -1,7 +1,15 @@
 import {config as rootConfig} from '../../eslint.config'
 import {composeConfig} from '../eslint-config/src'
 
-export default composeConfig(rootConfig).insertAfter('@bfra.me/ignores', {
-  name: '@bfra.me/create/ignores',
-  ignores: ['src/templates/**/package.json'],
-})
+export default composeConfig(rootConfig)
+  .insertAfter('@bfra.me/ignores', {
+    name: '@bfra.me/create/ignores',
+    ignores: ['src/templates/**/package.json'],
+  })
+  .append({
+    name: '@bfra.me/create/overrides',
+    files: ['src/templates/**/*.ts'],
+    rules: {
+      'no-console': 'off',
+    },
+  })
